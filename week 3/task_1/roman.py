736c43cd82facb2ee205e07fdbc84106d36219a7
Boris <151388715+AstrakovBA@users.noreply.github.com>
2025-03-05 18:58:11 +0300

    Update roman.py

--- week 3/task_1/roman.py
+++ week 3/task_1/roman.py
@@ -1,4 +1,4 @@
-'''Модуль с классом Roman'''
+"""Модуль с классом Roman для работы с римскими числами."""
 
 class Roman:
     """Класс для работы с римскими числами и поддержкой арифметических операций."""
@@ -16,7 +16,13 @@ class Roman:
         1000: 'M'
     }
 
-    def __init__(self, value: str | int):
+    def __init__(self, value: str | int) -> None:
+        """
+        Инициализация объекта Roman.
+
+        :param value: Римское число (str) или арабское число (int).
+        :raises ValueError: Если значение не является строкой или целым числом.
+        """
         if isinstance(value, str):
             self._value: int = self.roman_to_arabic(value)
         elif isinstance(value, int):
@@ -61,16 +67,69 @@ class Roman:
         return roman
 
     def __add__(self, other: 'Roman') -> 'Roman':
+        """
+        Сложение двух объектов Roman.
+
+        :param other: Второй объект Roman.
+        :return: Новый объект Roman с результатом сложения.
+        """
         return Roman(self._value + other._value)
 
     def __sub__(self, other: 'Roman') -> 'Roman':
+        """
+        Вычитание одного объекта Roman из другого.
+
+        :param other: Второй объект Roman.
+        :return: Новый объект Roman с результатом вычитания.
+        """
         return Roman(self._value - other._value)
 
     def __mul__(self, other: 'Roman') -> 'Roman':
+        """
+        Умножение двух объектов Roman.
+
+        :param other: Второй объект Roman.
+        :return: Новый объект Roman с результатом умножения.
+        """
         return Roman(self._value * other._value)
 
     def __truediv__(self, other: 'Roman') -> 'Roman':
+        """
+        Целочисленное деление одного объекта Roman на другой.
+
+        :param other: Второй объект Roman.
+        :return: Новый объект Roman с результатом деления.
+        """
         return Roman(self._value // other._value)
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление объекта Roman в виде римского числа.
+
+        :return: Римское число (str).
+        """
         return self.arabic_to_roman(self._value)
+
+    @property
+    def value(self) -> int:
+        """
+        Геттер для получения арабского значения числа.
+
+        :return: Арабское число (int).
+        """
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str | int) -> None:
+        """
+        Сеттер для установки нового значения числа.
+
+        :param new_value: Новое значение (str или int).
+        :raises ValueError: Если значение не является строкой или целым числом.
+        """
+        if isinstance(new_value, str):
+            self._value = self.roman_to_arabic(new_value)
+        elif isinstance(new_value, int):
+            self._value = new_value
+        else:
+            raise ValueError("Неверное значение! Введите арабское или римское число")
