cb6d87501830156517a99ab024abffc7870095c4
Boris <151388715+AstrakovBA@users.noreply.github.com>
2025-03-05 20:24:20 +0300

    Update main.py

--- week 4/task_3/main.py
+++ week 4/task_3/main.py
@@ -1,5 +1,3 @@
-from abc import ABC, abstractmethod
-from typing import Union
 import deposits
 # Пример использования
 deposit = choose_deposit('bonus', amount=100000, rate=0.05, period=3, bonus_threshold=50000, bonus_rate=0.1)
