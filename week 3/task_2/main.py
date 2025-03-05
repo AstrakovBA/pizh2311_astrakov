4a26704fe5b7f3881cd61ca6be1b813d5212e2ae
Boris <151388715+AstrakovBA@users.noreply.github.com>
2025-03-05 18:39:02 +0300

    Update and rename task_2.py to main.py

---
+++ week 3/task_2/main.py
@@ -0,0 +1,11 @@
+import order
+import pizzas
+import terminal
+
+def main():
+    """Основная функция для запуска терминала пиццерии."""
+    терминал = Терминал()
+    терминал.начать()
+
+if __name__ == "__main__":
+    main()
