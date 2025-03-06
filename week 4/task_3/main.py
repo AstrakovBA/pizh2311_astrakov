7b382e8e83db80e85ca5c9acf1db81538633bcb8
Boris <151388715+AstrakovBA@users.noreply.github.com>
2025-03-06 11:25:32 +0300

    Update main.py

--- week 4/task_3/main.py
+++ week 4/task_3/main.py
@@ -1,3 +1,11 @@
+"""
+Программирование на языке высокого уровня (Python).
+Задание №3
+Выполнил: Астраков Б. А.
+Группа: ПИЖ-б-о-23-1
+E-Mail: bororo8918@gmail.com
+"""
+
 import deposits
 # Пример использования
 deposit = choose_deposit('bonus', amount=100000, rate=0.05, period=3, bonus_threshold=50000, bonus_rate=0.1)
